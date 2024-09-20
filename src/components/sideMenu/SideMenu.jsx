c7784c3cbb978bcce3b5adc7420c1180fcd78a78
tomaskls <tomaskls@hotmail.com>
2024-09-20 16:35:02 +0300

    gallery links done

--- src/components/sideMenu/SideMenu.jsx
+++ src/components/sideMenu/SideMenu.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import './sideMenu.css';
+import { Link } from 'react-router-dom';
+
 
 const MenuIcon = () => (
   <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="30px" width="30px" xmlns="http://www.w3.org/2000/svg"><path d="M32 96v64h448V96H32zm0 128v64h448v-64H32zm0 128v64h448v-64H32z"></path></svg>
@@ -81,9 +83,12 @@ export const SidebarMenu = () => {
             <li>
               <Scissors />
               <a href="#design" onClick={closeMenu}>Modeliavimas</a></li>
-            <li>
+            <li className='liGallery'>
               <Scissors />
-              <a href="#" onClick={closeMenu}>Galerija</a></li>
+            <Link to="/gallery">
+              <span onClick={closeMenu}>Galerija</span>
+              </Link>
+              </li>
             <li>
               <Scissors />
               <a href="#" onClick={closeMenu}>D.U.K.</a></li>
