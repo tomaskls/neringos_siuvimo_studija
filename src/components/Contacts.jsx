b2fb672486e886ea56caab783f986bf49feeec76
tomaskls <tomaskls@hotmail.com>
2024-09-22 19:00:18 +0300

    contacts done

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -11,11 +11,21 @@ export function Contacts() {
             <div className={style.contacts}>
                 <h2 >Kontaktai</h2>
                 <a href="+37065073547">Skambinkite tel: +370 650 73547</a>
-                <p className="">Mus rasite adresu: <br/>Vytauto g. 80 <br/>Šiauliai</p>
-                    <p>Artimiausias nemokamas parkingas uz 200 metrų - rudės g.  14(maxima)</p>
-                </div>
+                <p className="">Mus rasite adresu: <br />Vytauto g. 80 <br />Šiauliai</p>
+                <p>Artimiausias nemokamas parkingas uz 200 metrų - rudės g.  14(maxima)</p>
             </div>
+            <iframe className={style.map}
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2235.234194280573!2d23.31294907676809!3d55.92797787848508!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e5e330649f95bb%3A0x37e61039db777fb7!2sNERINGOS%20SIUVIMO%20STUDIJA!5e0!3m2!1slt!2slt!4v1727020354296!5m2!1slt!2slt"
+                width="600"
+                height="450"
+                style={{ border: 0 }}
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+                </div>
 
 
-            )
+
+    )
 }
