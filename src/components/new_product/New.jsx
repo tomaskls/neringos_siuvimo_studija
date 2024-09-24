bbfdb5f8a3467d981332bc9a7ba1c292db70aa95
tomaskls <tomaskls@hotmail.com>
2024-09-24 18:03:18 +0300

    mobile heading colors

--- src/components/new_product/New.jsx
+++ src/components/new_product/New.jsx
@@ -11,7 +11,7 @@ export function New() {
             <a className={style.alink} id="new" href=""><img className={style.img} src={dzemperis} alt="ranku darbo flisiniai dzemperiai"/></a>
             <div className={style.contentNewItems}>
                 <h2 className={style.h2}>Norvegiški kalnai įkvėpė šildyti lietuviškas širdis</h2>
-                <p className={style.p}>Po kelionių per ledinius Norvegijos kalnus supratome, kad yra vienas dalykas, kurio lietuviams tikrai trūksta - flisinio jaukumo, kuris būtų nepamainomas drėgnomis ir vėsiomis dienomis. Grįžę namo, nusprendėme pasidalinti savo atradimu ir pradėjome gaminti flisinius džemperius, kurie yra tokie šilti, kad net pingvinai pavydėtų. Dar geriau - mes leidžiame jums tapti savo džemperio dizaineriu! Galite rinktis dydį, spalvą ir netgi pritaikyti detales pagal savo norus – nuo kišenių vietos iki užtrauktuko dydžio. Dabar jūs galite jaustis kaip tikri vikingai, bet su daugiau jaukumo ir visiškai individualizuotu stiliumi!</p>
+                <p className={style.p}>Po kelionių per ledinius Norvegijos kalnus supratome, kad yra vienas dalykas, kurio lietuviams tikrai trūksta - flisinio jaukumo, kuris būtų nepamainomas drėgnomis ir vėsiomis dienomis. Grįžę namo, nusprendėme pasidalinti savo atradimu ir pradėjome gaminti flisinius džemperius, kurie yra tokie šilti, kad net pingvinai pavydėtų. Dar geriau - mes leidžiame jums tapti savo džemperio dizaineriu! Galite rinktis dydį, spalvą ir netgi pritaikyti detales pagal savo norus – nuo kišenių vietos iki užtrauktuko dydžio!</p>
                 <p className={style.p}>
                     Pavyzdžius rasite :  
                     <Link 
