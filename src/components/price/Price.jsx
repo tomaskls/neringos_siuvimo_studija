4652739809ad6f8e49835a350c5247563f8d27a7
tomaskls <tomaskls@hotmail.com>
2024-10-17 18:37:51 +0300

    price update

--- src/components/price/Price.jsx
+++ src/components/price/Price.jsx
@@ -27,8 +27,8 @@ export function Price() {
                     <h2 className={style.h2}>Drabužių taisymo darbų kainoraštis</h2>
                     <div className={style.list}>
                         <h3 className={style.h3}>Kelnių trumpinimas :</h3>
-                        <p className={style.p}>Džinsų trumpinimas - 10€</p>
-                        <p className={style.p}>Kelnių trumpinimas mašina - 8€</p>
+                        <p className={style.p}>Džinsų trumpinimas - 10-15€</p>
+                        <p className={style.p}>Kelnių trumpinimas mašina - 10€</p>
                         <p className={style.p}>Kelnių trumpinimas su juostele - 12€</p>
                         <p className={style.p}>Kelnių trumpinimas su atvartais - 10€</p>
                         <p className={style.p}>Sportiniu kelnių su užtrauktukais - 12€</p>
@@ -48,16 +48,16 @@ export function Price() {
                 <div className={style.content2}>
                     <div className={style.list}>
                         <h3 className={style.h3}>Sijonų taisymas :</h3>
-                        <p className={style.p}>Sijono trumpinimas - 10€</p>
-                        <p className={style.p}>Sijono trumpinimas su pamušalu - 12€</p>
+                        <p className={style.p}>Sijono trumpinimas - 10-15€</p>
+                        <p className={style.p}>Sijono trumpinimas su pamušalu - 12-15€</p>
                         <p className={style.p}>Sijono siaurinimas - 15€</p>
                         <p className={style.p}>Sijono siaurinimas su pamušalu - 20€</p>
                     </div>
                     <div className={style.list}>
                         <h3 className={style.h3}>Švarkų taisymas:</h3>
                         <p className={style.p}>Švarko siaurinimas (1 siūlė)- 8€</p>
-                        <p className={style.p}>Švarko apačios trumpinimas - 15€</p>
-                        <p className={style.p}>Švarko rankovių trumpinimas - 10€</p>
+                        <p className={style.p}>Švarko apačios trumpinimas - 15-20€</p>
+                        <p className={style.p}>Švarko rankovių trumpinimas - 15€</p>
                         <p className={style.p}>Švarko rankovių perstatymas - 20€</p>
                         <p className={style.p}>Švarko su pamušalu siaurinimas (1 siūlė) - 10€</p>
                     </div>
@@ -65,9 +65,9 @@ export function Price() {
                     <div className={style.list}>
                         <h3 className={style.h3}>Suknelių taisymas :</h3>
                         <p className={style.p}>Suknelės siaurinimas per šonines siūles - 20€</p>
-                        <p className={style.p}>Suknelės apačios lenkimas - 10€</p>
+                        <p className={style.p}>Suknelės apačios lenkimas - 15€</p>
                         <p className={style.p}>Suknelės plačia apačia su pamušalu trumpinimas - 20€</p>
-                        <p className={style.p}>Suknelės petnešų trumpinimas - 8€</p>
+                        <p className={style.p}>Suknelės petnešų trumpinimas - 10€</p>
                         <p className={style.p}>Suknelės rankovių trumpinimas - 10€</p>
                         <p className={style.p}>Suknelės rankovių perstatymas - 15€</p>
                     </div>
@@ -90,19 +90,19 @@ export function Price() {
                     </div>
                     <div className={style.list}>
                         <h3 className={style.h3}>Pamušalų keitimas:</h3>
-                        <p className={style.p}>Paltui - 24€</p>
-                        <p className={style.p}>Puspalčiui - 20€</p>
-                        <p className={style.p}>Švarkui - 15€</p>
-                        <p className={style.p}>Sijonui - 10€</p>
+                        <p className={style.p}>Paltui - 30€</p>
+                        <p className={style.p}>Puspalčiui - 25€</p>
+                        <p className={style.p}>Švarkui - 20.4924C20€</p>
+                        <p className={style.p}>Sijonui - 15€</p>
                         <p className={style.p}>Suknelei - 15€</p>
-                        <p className={style.p}>Striukei - 20€</p>
+                        <p className={style.p}>Striukei - 25-35€</p>
                         <p className={style.p}>Kailiniams - 50€</p>
                     </div>
                     <div className={style.list}>
                         <h3 className={style.h3}>Užtrauktukų keitimas:</h3>
                         <p className={style.p}>Striukė (žieminė) - 25€</p>
                         <p className={style.p}>Striukė (plona) - 20€</p>
-                        <p className={style.p}>Vaikiška striukė - 12€</p>
+                        <p className={style.p}>Vaikiška striukė - 15€</p>
                         <p className={style.p}>Džinsų užtrauktuko keitimas - 10€</p>
                         <p className={style.p}>Sijono užtrauktuko keitimas - 10€</p>
                         <p className={style.p}>Vyr. kelnių užtrauktukas - 15€</p>
