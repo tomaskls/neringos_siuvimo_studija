e9c3df9e3e7e6f3ca913612ba748f02b7abb9c35
Tomas Kazlauskas <tomaskls@hotmail.com>
2024-10-18 13:25:40 +0300

    Update Sewing.jsx

--- src/components/sewing/Sewing.jsx
+++ src/components/sewing/Sewing.jsx
@@ -13,7 +13,7 @@ export function Sewing() {
         <div id='sewing' className={style.container}>
             <div><PictureSewing /></div>
             <div className={style.content}>
-                <h2 className={style.h2}>Ateik su idėja, išeik  su drabužiu !</h2>
+                <h2 className={style.h2}>Ateik su idėja, išeik su drabužiu!</h2>
                 <p className={style.p}>Kiekvienas turi savo unikalų stilių, bet ne visada lengva jį rasti parduotuvėje. Čia mes įžengiame į žaidimą - siuvame pagal jūsų norus ir poreikius. Nesvarbu, ar tai būtų kostiumas su per dideliais atlapais, suknelė, kuri privers suktis galvas kiekviename žingsnyje, ar džemperis, kuriame norėtumėt praleisti visą žiemą, mes pasirūpinsime, kad tai taptų realybe.</p>
                 <ul className={style.repairList}>
                     <li>Moteriškų drabužių siuvimas</li>
