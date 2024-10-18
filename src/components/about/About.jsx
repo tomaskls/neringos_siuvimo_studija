a796f4335b488feaf9797bc0433f4db6a27e3081
tomaskls <tomaskls@hotmail.com>
2024-10-18 13:58:17 +0300

    final before pages change

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -17,7 +17,7 @@ export function About() {
             </div>
             <div className={style.content}>
                 <h2 className={style.h2}>Apie Mus</h2>
-                <p className={style.p}>Siuvimo studija, kurioje siūlai visada randa kelią namo Nuo 2007 metų esame Šiaulių siuvimo meistrai, sugebantys iš bet kokios medžiagos sukurti šedevrą. Kai mūsų studija atveria duris, siūlai pradeda dainuoti, o adatos - šokti. Ateikite ir mes jums parodysime, kaip galima prikelti net seniausią audinį naujam gyvenimui!</p>
+                <p className={style.p}>Siuvimo studija, kurioje siūlai visada randa kelią namo. Nuo 2007 metų esame Šiaulių siuvimo meistrai, sugebantys iš bet kokios medžiagos sukurti šedevrą. Kai mūsų studija atveria duris, siūlai pradeda dainuoti, o adatos - šokti. Ateikite ir mes jums parodysime, kaip galima prikelti net seniausią audinį naujam gyvenimui!</p>
                 <div className={style.svg} ><Adata /></div>
             </div>
         </div>
