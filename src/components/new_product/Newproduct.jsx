48a7dd1d54fbbde8bbcb1a9782a681a1b1cc90fc
tomaskls <tomaskls@hotmail.com>
2024-10-20 18:24:36 +0300

    jmp

--- src/components/new_product/Newproduct.jsx
+++ src/components/new_product/Newproduct.jsx
@@ -1,6 +1,6 @@
 import style from './New.module.css';
-import { Link } from 'react-router-dom';
-import { useTouch } from '../Usetouch';
+// import { Link } from 'react-router-dom';
+// import { useTouch } from '../Usetouch';
 import { Smasina } from '../svg';
 import { useEffect } from 'react';
 
@@ -12,8 +12,13 @@ const PictureNew = () => {
    )
 };
 
+
+
 export function NewProduct() {
-    const { isTouched, handleTouchStart, handleTouchEnd } = useTouch();
+    // const { isTouched, handleTouchStart, handleTouchEnd } = useTouch();
+    // useEffect(() => {
+    //     window.scrollTo(0, 0); 
+    //   }, []);
     useEffect(() => {
         window.scrollTo(0, 0); 
       }, []);
@@ -26,7 +31,7 @@ export function NewProduct() {
             <div className={style.contentNewItems}>
                 <h2 className={style.h2}>Kalnai įkvėpė šildyti lietuviškas širdis</h2>
                 <p className={style.p}>Po kelionių per ledinius Norvegijos kalnus supratome, kad yra vienas dalykas, kurio lietuviams tikrai trūksta - flisinio jaukumo, kuris būtų nepamainomas drėgnomis ir vėsiomis dienomis. Grįžę namo, nusprendėme pasidalinti savo atradimu ir pradėjome gaminti flisinius džemperius, kurie yra tokie šilti, kad net pingvinai pavydėtų. Dar geriau - mes leidžiame jums tapti savo džemperio dizaineriu! Galite rinktis dydį, spalvą ir netgi pritaikyti detales pagal savo norus – nuo kišenių vietos iki užtrauktuko dydžio!</p>
-                <p className={style.p}>
+                {/* <p className={style.p}>
                     Pavyzdžius rasite :&nbsp;&nbsp;  
                     <Link
                         className={`${style.plink} ${isTouched ? style.touched : ''}`}
@@ -35,7 +40,7 @@ export function NewProduct() {
                         onTouchEnd={handleTouchEnd}
                     >Galerijoje
                     </Link>
-                </p>
+                </p> */}
                 <div className={style.svg}><Smasina /></div>
             </div>
         </div>
