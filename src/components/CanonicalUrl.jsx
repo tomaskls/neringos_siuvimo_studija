d1b4312a8557289bfff02f3ad40828e1762ede9c
tomaskls <tomaskls@hotmail.com>
2024-11-11 09:34:45 +0200

    can comp

--- src/components/CanonicalUrl.jsx
+++ src/components/CanonicalUrl.jsx
@@ -1,19 +1,25 @@
-import { Helmet} from "react-helmet-async"; 
+import { Helmet } from "react-helmet-async"; 
 
 const CanonicalUrl = () => {
-  const canonicalUrl = 'https://www.neringos-siuvimo-studija.lt';
-  return (
-    <Helmet>
-      <link rel="canonical" href={canonicalUrl} />
-      {[
-        'http://www.neringos-siuvimo-studija.lt',
-        'http://www.neringossiuvimostudija.lt',
-        'https://www.neringossiuvimostudija.lt'
-      ].map((url) => (
-        <link key={url} rel="alternate" href={url} />
-      ))}
-    </Helmet>
-  );
+ const canonicalUrl = 'https://www.neringos-siuvimo-studija.lt';
+
+ return (
+   <Helmet>
+     <link rel="canonical" href={canonicalUrl} />
+     {[
+       'http://neringos-siuvimo-studija.lt',
+       'https://neringos-siuvimo-studija.lt',
+       'http://www.neringos-siuvimo-studija.lt',
+       'http://neringossiuvimostudija.lt',
+       'https://neringossiuvimostudija.lt',
+       'http://www.neringossiuvimostudija.lt',
+       'https://www.neringossiuvimostudija.lt',
+       'https://siuvimo-studija-3.vercel.app'
+     ].map((url) => (
+       <link key={url} rel="alternate" href={url} />
+     ))}
+   </Helmet>
+ );
 };
 
 export default CanonicalUrl;
